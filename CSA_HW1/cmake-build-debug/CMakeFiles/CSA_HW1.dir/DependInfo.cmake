991dc2f3db2307e718b95a09a01b2909962008d0
AnnaSibirtseva <71088887+AnnaSibirtseva@users.noreply.github.com>
2021-10-08 21:13:26 +0300

    Create DependInfo.cmake

---
+++ CSA_HW1/cmake-build-debug/CMakeFiles/CSA_HW1.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/anias/CLionProjects/CSA_HW1/Animal.cpp" "C:/Users/anias/CLionProjects/CSA_HW1/cmake-build-debug/CMakeFiles/CSA_HW1.dir/Animal.cpp.obj"
+  "C:/Users/anias/CLionProjects/CSA_HW1/Beast.cpp" "C:/Users/anias/CLionProjects/CSA_HW1/cmake-build-debug/CMakeFiles/CSA_HW1.dir/Beast.cpp.obj"
+  "C:/Users/anias/CLionProjects/CSA_HW1/Bird.cpp" "C:/Users/anias/CLionProjects/CSA_HW1/cmake-build-debug/CMakeFiles/CSA_HW1.dir/Bird.cpp.obj"
+  "C:/Users/anias/CLionProjects/CSA_HW1/Container.cpp" "C:/Users/anias/CLionProjects/CSA_HW1/cmake-build-debug/CMakeFiles/CSA_HW1.dir/Container.cpp.obj"
+  "C:/Users/anias/CLionProjects/CSA_HW1/Fish.cpp" "C:/Users/anias/CLionProjects/CSA_HW1/cmake-build-debug/CMakeFiles/CSA_HW1.dir/Fish.cpp.obj"
+  "C:/Users/anias/CLionProjects/CSA_HW1/main.cpp" "C:/Users/anias/CLionProjects/CSA_HW1/cmake-build-debug/CMakeFiles/CSA_HW1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
