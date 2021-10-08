3fab782649a612ea40ebf1db49293aa8df1783ac
AnnaSibirtseva <71088887+AnnaSibirtseva@users.noreply.github.com>
2021-10-08 21:13:09 +0300

    Create cmake_clean.cmake

---
+++ CSA_HW1/cmake-build-debug/CMakeFiles/CSA_HW1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CSA_HW1.dir/Animal.cpp.obj"
+  "CMakeFiles/CSA_HW1.dir/Beast.cpp.obj"
+  "CMakeFiles/CSA_HW1.dir/Bird.cpp.obj"
+  "CMakeFiles/CSA_HW1.dir/Container.cpp.obj"
+  "CMakeFiles/CSA_HW1.dir/Fish.cpp.obj"
+  "CMakeFiles/CSA_HW1.dir/main.cpp.obj"
+  "CSA_HW1.exe"
+  "CSA_HW1.exe.manifest"
+  "CSA_HW1.lib"
+  "CSA_HW1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CSA_HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
